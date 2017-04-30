b9259d3edbe7c6ea4721bb994800f8e50b9104f7
Junior Samaroo <jns0987654321@gmail.com>
2017-04-30 16:28:50 -0400

    Nullability done

--- Nullability/ViewController.m
+++ Nullability/ViewController.m
@@ -14,9 +14,30 @@
 
 @implementation ViewController
 
+-(void)getName:(NSString * _Nullable)name {
+
+}
+
+-(int)sum:(nonnull NSNumber*)numA :(nonnull NSNumber*)numB {
+    int theSum = numA.intValue + numB.intValue;
+    return theSum;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSNumber *num1;
+    NSNumber *num2;
+    
+    int sum = [self sum:num1 :num2];
+    
+    NSLog(@"SUM: %d", sum); //prints 0, however is null
+    
+    [self getName:nil];
+    
+    // if passing null in objC will give warning, but swift will throw error at compile time
+    // use optionals in swift to say it is nullable
+    
 }
 
 
